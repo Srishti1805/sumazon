2d36c26e16730eaf5be07179c7073daa7c230ff4
Srishti1805 <72729278+Srishti1805@users.noreply.github.com>
2024-02-24 00:25:13 -0800

    feat:add order table in admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,10 +1,23 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
 
-from .models import Products
+from .models import Products, Order
 
 
 @admin.register(Products)
 class ProductsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'picture', 'price', 'description')
+    list_display = (
+        'id',
+        'name',
+        'picture',
+        'price',
+        'description',
+        'quantity',
+    )
     search_fields = ('name',)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'userId', 'incomingData')
+    list_filter = ('userId',)
