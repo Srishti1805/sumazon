652f097bb92cb078ce052c9fdaaaed4d122a4f70
Srishti1805 <srishti.adkar@gmail.com>
2024-03-02 09:51:57 +0530

    chore: resolve authentication class issue.

--- src/urls.py
+++ src/urls.py
@@ -36,7 +36,7 @@ schema_view = get_schema_view(
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
-    authentication_class=(JWTAuthentication,),
+    authentication_classes=(JWTAuthentication,),
 )
 
 
