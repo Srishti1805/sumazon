a882551be7fd9af5438bed82dfd30327fb745c5f
Srishti1805 <srishti.adkar@gmail.com>
2024-03-02 10:05:11 +0530

    feat: create order post api.

--- products/serializers.py
+++ products/serializers.py
@@ -1,22 +1,29 @@
 from .models import Products, Order
 from rest_framework import serializers
 
+
 class JSONSerializerField(serializers.Field):
     def to_internal_value(self, data):
         return data
+
     def to_representation(self, value):
         return value
+
 
 class ProductsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Products
-        fields = '__all__'
+        fields = "__all__"
+
 
 class OrderSerializer(serializers.ModelSerializer):
     incomingData = JSONSerializerField()
+
     class Meta:
         model = Order
-        fields = '__all__'
+        fields = "__all__"
 
 
-
+class ProductQuantitySerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    quantity = serializers.IntegerField()
--- products/views.py
+++ products/views.py
@@ -6,6 +6,7 @@ from rest_framework import status
 
 # Create your views here.
 
+
 class ProductsView(ReadOnlyModelViewSet):
     queryset = models.Products.objects.all()
     serializer_class = serializers.ProductsSerializer
@@ -20,12 +21,44 @@ class OrderView(ModelViewSet):
     filterset_class = filters.OrderFilter
     ordering = ("-id",)
 
-
     def get_queryset(self):
-        return models.Order.objects.filter(userId = self.request.user)
-    
+        return models.Order.objects.filter(userId=self.request.user)
+
     def create(self, request):
-        pass
+        data = request.data
+        serializer = serializers.ProductQuantitySerializer(data=data, many=True)
+        products = []
+        if serializer.is_valid():
+            for item in serializer.validated_data:
+                product_id = item["id"]
+                quantity = item["quantity"]
+                try:
+                    product = models.Products.objects.get(id=product_id)
+                except models.Products.DoesNotExist:
+                    return Response(
+                        f"Product with id {product_id} does not exist.",
+                        status=status.HTTP_404_NOT_FOUND,
+                    )
+                if quantity > product.quantity:
+                    return Response(
+                        f"Quantity for product {product.name} is greater than current stock.",
+                        status=status.HTTP_400_BAD_REQUEST,
+                    )
+                product.quantity -= quantity
+                products.append(product)
+            models.Products.objects.bulk_update(products, ["quantity"])
+
+            serializer = serializers.OrderSerializer(
+                data={"incomingData": data, "userId": request.user.id}
+            )
+            if serializer.is_valid():
+                serializer.save()
+                return Response(
+                    "Product quantities updated successfully.",
+                    status=status.HTTP_200_OK,
+                )
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def update(self, request, pk=None):
         return Response("METHOD NOT ALLOWED", status=status.HTTP_405_METHOD_NOT_ALLOWED)
